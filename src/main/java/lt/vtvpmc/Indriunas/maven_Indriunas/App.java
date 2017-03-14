a79cf0af2a46a8771aa1a063ecd70cf0b20fb621
Simonas <simonas.indriunas@gmail.com>
2017-03-14 09:56:04 +0200

    uzduotis 1

---
+++ src/main/java/lt/vtvpmc/Indriunas/maven_Indriunas/App.java
@@ -0,0 +1,13 @@
+package lt.vtvpmc.Indriunas.maven_Indriunas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
